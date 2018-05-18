9def7c34106d31b4bcd0e144eaa8c5af5c142b23
Sebastian S <su4lfred@gmail.com>
2018-05-18 09:25:12 +0200

    added counter

--- index.php
+++ index.php
@@ -13,28 +13,27 @@
 	<?php
 
 	$servermac = "aa:bb:cc:dd:ee:ff";
-	$serverip = "192.168.2.250";
+	$serverip = "192.168.2.123";
 	$name = "Neo";
 
 	$ip = isset($_SERVER['HTTP_CLIENT_IP'])?$_SERVER['HTTP_CLIENT_IP']:isset($_SERVER['HTTP_X_FORWARDED_FOR'])?$_SERVER['HTTP_X_FORWARDED_FOR']:$_SERVER['REMOTE_ADDR'];
+	$counter = $_GET['c'];
 
 	if ($_GET['wake']) {
 		exec ("wakeonlan $servermac");
-		$status = "packets sent.";
-		$statusmsg = "Knock, knock, $name.";
 	}
 
 	$pinginfo = exec("ping -c 1 $serverip");
 
-	if ($pinginfo != "" ) {
-		$status = "awaked.";
-		$statusmsg = "The Matrix has you.";
-	}
-
 	if ($pinginfo == "" AND $_GET['wake']) {
 		$page = $_SERVER['PHP_SELF'];
 		$sec = "1";
-		header("Refresh: $sec; url=?wake=true");
+		if (empty($counter)){
+			$counter = 1;
+		}else{
+			$counter++;
+		}
+		header("Refresh: $sec; url=?wake=true&c=$counter");
 	}
 
 	?>
@@ -52,15 +51,24 @@
 		<p class="output"></p>
 		<p class="output">Call trans opt: received. <?php echo date("c"); ?></p>
 		<p class="output">Trace program: running. <?php echo $ip ?></p>
-		<p class="output"></p>
 
-		<?php if ($status != "") { ?>
-			<p class="output">Status: <?php echo $status ?></p>
-			<p class="output"><?php echo $statusmsg ?></a></p>
-		<?php }
-		else { ?>
+		<?php if ($_GET['wake']) { ?>
+			<p class="output"></p>
+			<p class="output">Knock, knock, <?php echo $name ?>.</a></p>
+			<p class="output">Wake process: running. <?php echo $counter ?></p>
+		<?php } ?>
+
+		<?php if ($pinginfo != "") { ?>
+			<p class="output"></p>
+			<p class="output">Status: awaked.</a></p>
+			<p class="output">The Matrix has you.</p>
+		<?php } ?>
+
+		<?php if ($pinginfo == "" AND $_GET['wake'] == null) { ?>
+			<p class="output"></p>
 			<p class="output"><a href="?wake=true"">Wake up, <?php echo $name ?></a> ... follow the white rabbit.</a></p>
 		<?php } ?>
+
 	</div>
 
 	<script  src="js/index.js"></script>
